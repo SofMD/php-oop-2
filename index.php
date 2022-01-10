966b7203169a3a5e74dff4084ff768bf87838dcb
sofMD <xsofiamaria.32@gmail.com>
2022-01-10 17:29:15 +0100

    metodi, classe utenti

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci 
 Eseguire poi degli output istanziando oggetti delle varie classi
 */
 
-
+//------PRODOTTI------
 class Products {
     //proprietà
     public $nomeProdotto;
@@ -18,6 +18,14 @@ class Products {
         $this -> nomeProdotto = $nomeProdotto;
         $this -> prezzo = $prezzo;
         $this -> disponibilità = $disponibilità;
+    }
+
+    public function contDisp(){
+        if ($this->disponibilità === true){
+            echo '<h4 style="color:green;"> articolo disponibile </h4> ';
+        }else{
+            echo '<h4 style="color:red;"> articolo NON disponibile </h4> ';
+        }
     }
 }
 
@@ -34,9 +42,15 @@ class secondHandProducts extends Products
 
         $this -> condizioni = $condizioni;
     }
+
+    public function getcondizioni(){
+        if ($this->condizioni === 'nuovo'){
+            echo '<div style=" display: inline-block;">CONDIZIONI:</div> <div style="background-color:green; width:20px; height: 20px;  display: inline-block;">  </div> ';
+        }elseif($this->condizioni === 'buone'){
+            echo '<div style=" display: inline-block;">CONDIZIONI:</div> <div style="background-color:orange; width:20px; height: 20px;  display: inline-block;">  </div> ';
+        }
+    }
 }
-
-
 
 
 
@@ -45,14 +59,80 @@ class secondHandProducts extends Products
 // prodotto 1
 $shoes = new Products('shoes', 78, true);
 var_dump($shoes);
+$shoes -> contDisp();
+
+echo '<br> <hr>';
 
 // prodotto 2
 $jacket = new Products('jacket', 93, false);
 var_dump($jacket);
+$jacket -> contDisp();
 
-echo '<br>';
+echo '<br> <hr>';
 
 //INSTANZE da classe estesa
 
-$sweatshirt = new secondHandProducts('sweatshirt', 45, true, 'ottimo');
+$sweatshirt = new secondHandProducts('sweatshirt', 45, true, 'nuovo');
 var_dump($sweatshirt);
+$sweatshirt -> contDisp();
+$sweatshirt ->getcondizioni();
+
+echo '<br><hr>';
+
+$trousers = new secondHandProducts('trousers', 39, false, 'buone');
+var_dump($trousers);
+$trousers -> contDisp();
+$trousers ->getcondizioni();
+
+
+
+
+// -----UTENTI------
+
+class User {
+    public $name;
+    public $lastName;
+    public $age;
+
+    //metodi
+    public function __construct($name, $lastName, $age) {
+        $this -> name = $name;
+        $this -> lastName = $lastName;
+        $this -> age = $age;
+    }
+}
+
+//ereditarietà
+class premiumUsers extends User 
+{
+    //proprietà
+    public $level;
+    
+
+    // costruttore
+    public function __construct($name, $lastName, $age, $level){
+
+        parent::__construct($name, $lastName, $age);
+
+        $this -> level = $level;
+    }
+
+    public function utenteType(){
+        if ($this->level === 'premium'){
+            echo '<h4>Sei un utente premium </h4>';
+        }
+    }
+}
+
+
+// instanze
+echo '<br><hr>';
+echo '<br>';
+
+$user1 = new User('Carlo', 'Bianchi', 26 );
+var_dump($user1);
+
+echo '<br><hr>';
+$userPremium = new premiumUsers('Francesca', 'Rossi', 42, 'premium');
+var_dump($userPremium);
+$userPremium ->utenteType();
